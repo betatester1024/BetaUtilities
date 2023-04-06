6dfbd30086d455084ecb8998de98691cce675769
SarahisCode <111197143+SarahisCode@users.noreply.github.com>
2023-04-06 09:42:54 -0700

    eliminate K

--- EEHandler.ts
+++ EEHandler.ts
@@ -1,10 +1,10 @@
-import {K} from './consts';
+import {uDB} from './consts';
 
 export async function EE(getQ:boolean, callback:(status:string, data:any, token:string)=>any, token:string, newStr:string) {
-  let obj = await K.uDB.findOne({fieldName:"EE"});
+  let obj = await uDB.findOne({fieldName:"EE"});
   if (getQ) callback("SUCCESS", {data:obj?obj.data:""}, token);
   else {
-    await K.uDB.updateOne({fieldName:"EE"}, {
+    await uDB.updateOne({fieldName:"EE"}, {
       $set:{data:newStr}
     }, {upsert:true})
     callback("SUCCESS", null, token);

13d2b3181ddfe6c0b5dc641106caf59fcdd0b1b3
John Doe <betatester1025@protonmail.com>
2023-04-06 14:11:57 -0400

    Revert "Revert "Eliminate K""

--- delacc.ts
+++ delacc.ts
@@ -1,26 +1,26 @@
-import {K} from './consts';
+import {userRegex, authDB} from './consts';
 const argon2 = require('argon2');
 
 export async function delAcc(user:string, pass:string, callback:(status:string, data:any, token:string)=>any, token:string) {
-  let tokenData:{associatedUser:string, expiry:number} = await K.authDB.findOne({fieldName:"Token", token:token});
+  let tokenData:{associatedUser:string, expiry:number} = await authDB.findOne({fieldName:"Token", token:token});
   if (!tokenData) {
     callback("ERROR", {error:"Cannot update user information: Your session could not be found!"}, "")
     return;
   }
-  if (!user.match(K.userRegex)) {
+  if (!user.match(userRegex)) {
     callback("ERROR", {error:"Invalid user string!"}, token)
     return;
   }
   
-  let usrInfo = await K.authDB.findOne({fieldName:"UserData", user:{$eq:user}}) as {pwd:string, permLevel:number};
+  let usrInfo = await authDB.findOne({fieldName:"UserData", user:{$eq:user}}) as {pwd:string, permLevel:number};
   if (!usrInfo) {
     callback("ERROR", {error:"No such user!"}, token);
     return;
   }
-  let loginInfo:{permLevel:number}= await K.authDB.findOne({fieldName:"UserData", user:tokenData.associatedUser});
+  let loginInfo:{permLevel:number}= await authDB.findOne({fieldName:"UserData", user:tokenData.associatedUser});
   if (loginInfo.permLevel>=2) { // admin override
-    await K.authDB.deleteOne({fieldName:"Token", token:token})
-    await K.authDB.deleteOne({fieldName:"UserData", user:user});
+    await authDB.deleteOne({fieldName:"Token", token:token})
+    await authDB.deleteOne({fieldName:"UserData", user:user});
     callback("SUCCESS", null, token);
     return;
   }
@@ -29,8 +29,8 @@ export async function delAcc(user:string, pass:string, callback:(status:string, 
     return;
   }
   else if (await argon2.verify(usrInfo.pwd, pass)) {
-    await K.authDB.deleteOne({fieldName:"Token", token:token})
-    await K.authDB.deleteOne({fieldName:"UserData", user:user});
+    await authDB.deleteOne({fieldName:"Token", token:token})
+    await authDB.deleteOne({fieldName:"UserData", user:user});
     callback("SUCCESS", null, "");
     return;
   } else {
--- validateLogin.ts
+++ validateLogin.ts
@@ -1,9 +1,9 @@
 const argon2 = require('argon2');
-import {K} from './consts';
+import {authDB, userRegex, hashingOptions} from './consts';
 const crypto = require("crypto");
 
 export async function validateLogin(user:string, pwd:string, callback:(status:string, data:any, token:string)=>any, token:string) {
-  if (!user.match(K.userRegex)) {
+  if (!user.match(userRegex)) {
     callback("ERROR", {error:"Invalid user string!"}, token)
     return;
   }
@@ -11,15 +11,15 @@ export async function validateLogin(user:string, pwd:string, callback:(status:st
     callback("ERROR", {error:"No password provided!"}, token)
     return;
   }
-  let usrInfo = await K.authDB.findOne({fieldName:"UserData", user:{$eq:user}}) as {pwd:string, permLevel:number};
+  let usrInfo = await authDB.findOne({fieldName:"UserData", user:{$eq:user}}) as {pwd:string, permLevel:number};
   if (!usrInfo) {
     callback("ERROR", {error:"No such user!"}, token); // keep the original token.
     return;
   }
   else if (await argon2.verify(usrInfo.pwd, pwd)) {
     let uuid = crypto.randomUUID() // gen new token
-    let userData:{permLevel:number} = await K.authDB.findOne({fieldName:"UserData", user:user});
-    await K.authDB.insertOne({fieldName:"Token", associatedUser:user, token:uuid, expiry: Date.now()+K.expiry[userData.permLevel]})
+    let userData:{permLevel:number} = await authDB.findOne({fieldName:"UserData", user:user});
+    await authDB.insertOne({fieldName:"Token", associatedUser:user, token:uuid, expiry: Date.now()+K.expiry[userData.permLevel]})
     callback("SUCCESS", {perms: usrInfo.permLevel}, uuid);
     return;
   } else {
@@ -29,7 +29,7 @@ export async function validateLogin(user:string, pwd:string, callback:(status:st
 }
 
 export async function signup(user:string, pwd:string, callback:(status:string, data:any, token:string)=>any, token:string) {
-  if (!user.match(K.userRegex)) {
+  if (!user.match(userRegex)) {
     callback("ERROR", {error:"Invalid user string!"}, token)
     return;
   }
@@ -37,14 +37,14 @@ export async function signup(user:string, pwd:string, callback:(status:string, d
     callback("ERROR", {error:"No password provided!"}, token)
     return;
   }
-  let usrInfo = await K.authDB.findOne({fieldName:"UserData", user:user}) as {pwd:string, permLevel:number};
+  let usrInfo = await authDB.findOne({fieldName:"UserData", user:user}) as {pwd:string, permLevel:number};
   if (usrInfo) {
     callback("ERROR", {error:"User is registered"}, token) // keep the original token.
     return;
   }
   else {
-    let hash = await argon2.hash(pwd, K.hashingOptions); 
-    await K.authDB.insertOne({fieldName:"UserData", user:user, pwd:hash, permLevel: 1});
+    let hash = await argon2.hash(pwd, hashingOptions); 
+    await authDB.insertOne({fieldName:"UserData", user:user, pwd:hash, permLevel: 1});
     validateLogin(user, pwd, callback, token);
     return;
   }
@@ -52,14 +52,14 @@ export async function signup(user:string, pwd:string, callback:(status:string, d
 
 export async function logout(callback:(status:string, data:any, token:string)=>any, token:string, allaccsQ:boolean=false) {
   if (allaccsQ) {
-    let userData:{associatedUser:string} = await K.authDB.findOne({fieldName:"Token", token:token});
+    let userData:{associatedUser:string} = await authDB.findOne({fieldName:"Token", token:token});
     if (!userData) {
-      await K.authDB.deleteOne({fieldName:"Token", token:token});
+      await authDB.deleteOne({fieldName:"Token", token:token});
       callback("ERROR", {error:"Cannot find your session. Logged you out."}, token)
       return;
     }
-    await K.authDB.deleteMany({fieldName:"Token", associatedUser:userData.associatedUser});
+    await authDB.deleteMany({fieldName:"Token", associatedUser:userData.associatedUser});
   }
-  await K.authDB.deleteOne({fieldName:"Token", token:token});
+  await authDB.deleteOne({fieldName:"Token", token:token});
   callback("SUCCESS", null, "");
 }

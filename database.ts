045340a3b802264a4deea7d3d5a5955c26959d03
betatester1024 <betatester1025@protonmail.com>
2023-04-12 17:56:04 +0000

    minor commit, feel free to ignore

--- database.ts
+++ database.ts
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 import {authDB, msgDB} from './consts';
 
 // Replace the uri string with your connection string.
-const uri =
+const uri = 
   // `mongodb+srv://SystemLogin:${process.env['dbPwd']}@betaos-datacluster00.d8o7x8n.mongodb.net/?retryWrites=true&w=majority`
   "mongodb://SystemLogin:"+process.env['dbPwd']+"@ac-rz8jdrl-shard-00-00.d8o7x8n.mongodb.net:27017,ac-rz8jdrl-shard-00-01.d8o7x8n.mongodb.net:27017,ac-rz8jdrl-shard-00-02.d8o7x8n.mongodb.net:27017/?ssl=true&replicaSet=atlas-3yyxq8-shard-0&authSource=admin&retryWrites=true&w=majority";
   // "mongodb+srv://SystemLogin:"+process.env['dbPwd']+"@betaos-datacluster00.d8o7x8n.mongodb.net/?retryWrites=true&w=majority";
